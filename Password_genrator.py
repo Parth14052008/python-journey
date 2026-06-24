15a64e6de7abf808dea3f6bd48747fe7cf9b59b8
Parth Sachdeva <88874298+Parth14052008@users.noreply.github.com>
2026-06-24 16:25:46 +0530

    Add files via upload

---
+++ Password_genrator.py
@@ -0,0 +1,38 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+import random
+mix_of_everything = random.choices(letters,k=nr_letters) + random.choices(symbols,k=nr_symbols) + random.choices(numbers,k=nr_numbers)
+print(mix_of_everything)
+
+
+
+random.shuffle(mix_of_everything) 
+    
+print(mix_of_everything)
+
+
+for i in mix_of_everything:
+    print(i,end="")
+
+
+print("This your password which i can say strong")
+
+
+
+
+
+
+
+
+
+   
